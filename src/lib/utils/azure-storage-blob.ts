db6c8f4c8a624ea1c25d8d6e7083d7d1b2bd7503
Tony Jara <340studiospy@gmail.com>
2022-11-17 12:38:20 -0300

    fixed image upload

---
+++ src/lib/utils/azure-storage-blob.ts
@@ -0,0 +1,69 @@
+// ./src/azure-storage-blob.ts
+
+// <snippet_package>
+// THIS IS SAMPLE CODE ONLY - NOT MEANT FOR PRODUCTION USE
+import type { ContainerClient } from '@azure/storage-blob';
+import { BlobServiceClient } from '@azure/storage-blob';
+
+const containerName = `tutorial-container`; //name of folder
+const sasToken = process.env.NEXT_PUBLIC_STORAGE_SASTOKEN;
+const storageAccountName = process.env.NEXT_PUBLIC_STORAGE_RESOURCE_NAME;
+
+// Feature flag - disable storage feature to app if not configured
+export const isStorageConfigured = () => {
+  return !storageAccountName || !sasToken ? false : true;
+};
+
+// return list of blobs in container to display
+const getBlobsInContainer = async (containerClient: ContainerClient) => {
+  const returnedBlobUrls: string[] = [];
+
+  // get list of blobs in container
+  // eslint-disable-next-line
+  for await (const blob of containerClient.listBlobsFlat()) {
+    // if image is public, just construct URL
+    returnedBlobUrls.push(
+      `https://${storageAccountName}.blob.core.windows.net/${containerName}/${blob.name}`
+    );
+  }
+
+  return returnedBlobUrls;
+};
+
+const createBlobInContainer = async (
+  containerClient: ContainerClient,
+  file: File
+) => {
+  // create blobClient for container
+  const blobClient = containerClient.getBlockBlobClient(file.name);
+
+  // set mimetype as determined from browser with file upload control
+  const options = { blobHTTPHeaders: { blobContentType: file.type } };
+
+  // upload file
+  await blobClient.uploadData(file, options);
+};
+
+const uploadFileToBlob = async (file: File | null): Promise<string[]> => {
+  if (!file) return [];
+
+  // get BlobService = notice `?` is pulled out of sasToken - if created in Azure portal
+  const blobService = new BlobServiceClient(
+    `https://${storageAccountName}.blob.core.windows.net/?${sasToken}`
+  );
+
+  // get Container - full public read access
+  const containerClient: ContainerClient =
+    blobService.getContainerClient(containerName);
+  await containerClient.createIfNotExists({
+    access: 'container',
+  });
+
+  // upload file
+  await createBlobInContainer(containerClient, file);
+
+  // get list of blobs in container
+  return getBlobsInContainer(containerClient);
+};
+
+export default uploadFileToBlob;
