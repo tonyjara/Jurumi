35ac65f49aaac0c339625a282602b7e19ebd1729
Tony Jara <340studiospy@gmail.com>
2022-11-17 15:03:17 -0300

    staging first

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -69,7 +69,7 @@ const Home = () => {
   );
   return (
     <div>
-      <h1>WELCOME HOME</h1>
+      <h1>WELCOME HOME ON STAGING</h1>
       <Button onClick={() => signOut()}>Signout</Button>
       <Divider mt={100} />
       <div>
