d31597aaa3cde1c3e5cff4a32aef213a96c6a928
Tony Jara <340studiospy@gmail.com>
2022-12-15 15:20:58 -0300

    working on taxpayer controller

--- src/components/FormControlled/FormControlledTaxPayerId.tsx
+++ src/components/FormControlled/FormControlledTaxPayerId.tsx
@@ -9,14 +9,21 @@ import {
   FormErrorMessage,
   InputGroup,
   InputRightElement,
+  Spinner,
+  Flex,
 } from '@chakra-ui/react';
-import React from 'react';
+import axios from 'axios';
+import { Select } from 'chakra-react-select';
+import { debounce } from 'lodash';
+import React, { useEffect, useState } from 'react';
 import type {
   Control,
+  ControllerRenderProps,
   FieldErrorsImpl,
   FieldValues,
   Path,
 } from 'react-hook-form';
+import { useWatch } from 'react-hook-form';
 import { Controller } from 'react-hook-form';
 
 interface InputProps<T extends FieldValues> {
@@ -34,38 +41,93 @@ const FormControlledTaxPayerId = <T extends FieldValues>(
   props: InputProps<T>
 ) => {
   const { control, name, errors, autoFocus } = props;
+  const [loading, setLoading] = useState(false);
 
-  // const res = (await axios(`https://rucs.datospy.org/api/${idCard}`)) as {
-  //   data: datosPyCustomer[];
-  // };
+  const watchTaxpayerId = useWatch({ control, name });
+
+  const getDataFromDatosPy = async (id: string) => {
+    try {
+      setLoading(true);
+      const { data } = await axios(`https://rucs.datospy.org/api/${id}`, {
+        timeout: 5000,
+      });
+      console.log(data);
+
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+      return null;
+    }
+  };
+
+  const debouncedQuery = debounce(
+    async () => getDataFromDatosPy(watchTaxpayerId),
+    500
+  );
+
+  useEffect(() => {
+    if (watchTaxpayerId.length > 5) {
+      // debounce(getDataFromDatosPy(watchTaxpayerId), 500)
+      debouncedQuery();
+    }
+
+    return () => {};
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [watchTaxpayerId]);
+
+  const handleOnChange = (e: any, field: ControllerRenderProps<T, Path<T>>) => {
+    return field.onChange(e?.value ?? '');
+  };
+
+  const handleValue = (field: ControllerRenderProps<T, Path<T>>) => {
+    // return [].find((x) => x.value === field.value) ?? {};
+
+    return field.value;
+  };
+
+  {
+    /* <Input
+    borderColor={'gray.300'}
+    maxLength={20}
+    value={field.value}
+    onChange={field.onChange}
+    autoFocus={autoFocus}
+   
+  /> */
+  }
   return (
     <FormControl isInvalid={!!errors[name]}>
       <FormLabel fontSize={'md'} color={'gray.500'}>
         Ruc del contribuyente
       </FormLabel>
+
       <Controller
         control={control}
         name={name}
         render={({ field }) => (
-          <InputGroup>
-            <Input
-              borderColor={'gray.300'}
-              maxLength={20}
-              value={field.value}
-              onChange={field.onChange}
-              autoFocus={autoFocus}
-            />
-
-            <InputRightElement pointerEvents={'none'}>
-              <Search2Icon />
-            </InputRightElement>
-          </InputGroup>
+          <Select
+            instanceId={name}
+            options={[]}
+            onChange={(e) => {
+              handleOnChange(e, field);
+            }}
+            value={handleValue(field)}
+            noOptionsMessage={() => 'No hay opciones.'}
+            placeholder=""
+            isClearable
+          />
         )}
       />
 
+      {/* <InputRightElement pointerEvents={'none'}>
+          {!loading && <Search2Icon />}
+          {loading && <Spinner />}
+        </InputRightElement> */}
+
       {!errors[name] ? (
         <FormHelperText color={'gray.500'}>
-          Ingrese el ruc y aguarde la busqueda.
+          Ingrese el ruc y aguarde la busqueda
         </FormHelperText>
       ) : (
         //@ts-ignore
