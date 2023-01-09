ff813c1b4eb255cbd5d4adbc39ef9388eaa054f1
nyto <340studiospy@gmail.com>
2023-01-09 01:07:46 -0300

    cmooon

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 // import { WEB_URL } from './data/environments';
 // import { getToken } from 'next-auth/jwt';
