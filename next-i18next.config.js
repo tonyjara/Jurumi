a0c5f755b12549f6a3b2e7e326dcb9cef264bbb1
nyto <340studiospy@gmail.com>
2023-01-09 01:42:55 -0300

    added path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'es',
     locales: ['en', 'es'],
+    localePath: path.resolve('./public/locales'),
     // reloadOnPrerender: process.env.NODE_ENV === 'development',
   },
 };
