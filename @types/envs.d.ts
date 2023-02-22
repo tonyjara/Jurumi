fbe3ded8809984cfa504f145cee35395252700ba
nyto <340studiospy@gmail.com>
2023-02-22 16:58:24 -0300

    added patch to save taxpayers

--- @types/envs.d.ts
+++ @types/envs.d.ts
@@ -1,42 +1,43 @@
 declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      //PUBLIC
-      NEXT_PUBLIC_WEB_URL: string;
-      //AUTH
-      NEXTAUTH_SECRET: string;
-      NEXTAUTH_URL: string;
-      JWT_SECRET: string;
-      //AZURE
-      STORAGE_RESOURCE_NAME: string;
-      AZURE_STORAGE_ACCESS_KEY: string;
+    namespace NodeJS {
+        interface ProcessEnv {
+            //PUBLIC
+            NEXT_PUBLIC_WEB_URL: string;
+            //AUTH
+            NEXTAUTH_SECRET: string;
+            NEXTAUTH_URL: string;
+            JWT_SECRET: string;
+            //AZURE
+            STORAGE_RESOURCE_NAME: string;
+            AZURE_STORAGE_ACCESS_KEY: string;
 
-      // SLACK
-      SLACK_BOT_TOKEN: string;
-      // FIREBASE
+            // SLACK
+            SLACK_BOT_TOKEN: string;
+            // FIREBASE
 
-      NEXT_PUBLIC_FB_API_KEY: string;
-      NEXT_PUBLIC_FB_AUTH_DOMAIN: string;
-      NEXT_PUBLIC_FB_PROJECT_ID: string;
-      NEXT_PUBLIC_FB_STORAGE_BUCKET: string;
-      NEXT_PUBLIC_FB_MESSAGING_SENDER_ID: string;
-      NEXT_PUBLIC_FB_APP_ID: string;
-      NEXT_PUBLIC_FB_MEASUREMENT_ID: string;
+            NEXT_PUBLIC_FB_API_KEY: string;
+            NEXT_PUBLIC_FB_AUTH_DOMAIN: string;
+            NEXT_PUBLIC_FB_PROJECT_ID: string;
+            NEXT_PUBLIC_FB_STORAGE_BUCKET: string;
+            NEXT_PUBLIC_FB_MESSAGING_SENDER_ID: string;
+            NEXT_PUBLIC_FB_APP_ID: string;
+            NEXT_PUBLIC_FB_MEASUREMENT_ID: string;
 
-      FCM_SERVER_KEY: string;
+            FCM_SERVER_KEY: string;
 
-      NEXT_PUBLIC_FB_MESSAGING_KEY: string;
+            NEXT_PUBLIC_FB_MESSAGING_KEY: string;
 
-      //SENDGRID
+            //SENDGRID
 
-      SENDGRID_API_KEY: string;
-      SENDGRID_FROM: string;
+            SENDGRID_API_KEY: string;
+            SENDGRID_FROM: string;
 
-      // OCR SERVER
+            // OCR SERVER
 
-      MS_API_KEY: string;
-      MS_OCR_URL: string;
+            MS_API_KEY: string;
+            MS_OCR_URL: string;
+            AMQP_URL: string;
+        }
     }
-  }
 }
-export {};
+export { };
--- components/Modals/imageEnlarge.modal.tsx
+++ components/Modals/imageEnlarge.modal.tsx
@@ -1,97 +1,98 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-  Flex,
-  Modal,
-  Image,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalOverlay,
-  Box,
-  Text,
-  ButtonGroup,
-  Button,
-} from '@chakra-ui/react';
+    Flex,
+    Modal,
+    Image,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalOverlay,
+    Box,
+    Text,
+    ButtonGroup,
+    Button,
+} from "@chakra-ui/react";
 
 import {
-  TransformWrapper,
-  TransformComponent,
-} from '@pronestor/react-zoom-pan-pinch';
-import { trpcClient } from '@/lib/utils/trpcClient';
-import { handleUseMutationAlerts } from '../Toasts & Alerts/MyToast';
+    TransformWrapper,
+    TransformComponent,
+} from "@pronestor/react-zoom-pan-pinch";
+import { trpcClient } from "@/lib/utils/trpcClient";
+import { handleUseMutationAlerts } from "../Toasts & Alerts/MyToast";
 
 const ImageEnlargeModal = ({
-  url,
-  imageName,
-  isOpen,
-  onClose,
-  text,
-  facturaNumber,
+    url,
+    imageName,
+    isOpen,
+    onClose,
+    text,
+    facturaNumber,
 }: {
-  url?: string;
-  imageName?: string;
-  facturaNumber?: string;
-  text?: string;
-  isOpen: boolean;
-  onClose: () => void;
+    url?: string;
+    imageName?: string;
+    facturaNumber?: string;
+    text?: string;
+    isOpen: boolean;
+    onClose: () => void;
 }) => {
-  const [disableButton, setDisableButton] = useState(false);
-  const { mutate } = trpcClient.gallery.scanImage.useMutation(
-    handleUseMutationAlerts({
-      successText: 'Enviado al servidor 🤖 podria tardar un minuto 🕰️',
-      callback: () => {},
-    })
-  );
+    const [disableButton, setDisableButton] = useState(false);
+    const { mutate } = trpcClient.gallery.scanImage.useMutation(
+        handleUseMutationAlerts({
+            successText: "Enviado al servidor 🤖 podria tardar un minuto 🕰️",
+            callback: () => { },
+        })
+    );
 
-  const handleImageScan = async () => {
-    if (!imageName) return;
-    mutate({ imageName: imageName });
+    const handleImageScan = async () => {
+        if (!imageName) return;
+        mutate({ imageName: imageName });
 
-    setDisableButton(true);
-    setTimeout(() => {
-      setDisableButton(false);
-    }, 20000);
-  };
+        setDisableButton(true);
+        setTimeout(() => {
+            setDisableButton(false);
+        }, 20000);
+    };
 
-  return (
-    <Modal size="5xl" isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalCloseButton />
-        <ModalBody>
-          <Flex flexDir={{ base: 'column', md: 'row' }}>
-            <Box w={{ base: '100%', md: '50%' }}>
-              <TransformWrapper>
-                <TransformComponent>
-                  <Image alt="expandable image" src={url} />
-                </TransformComponent>
-              </TransformWrapper>
-            </Box>
-            <Box
-              w={{ base: '100%', md: '50%' }}
-              p="20px"
-              minW={{ base: '0px', md: '400px' }}
-            >
-              <ButtonGroup mb="20px">
-                <Button isDisabled={disableButton} onClick={handleImageScan}>
-                  Escanear
-                </Button>
-              </ButtonGroup>
-              <Text mb={'10px'} fontSize={'xl'}>
-                Nombre: {imageName}
-              </Text>
-              <Text mb={'10px'} fontSize={'xl'}>
-                Número de factura: {facturaNumber}
-              </Text>
-              <Text mb={'10px'} fontSize={'xl'}>
-                Texto: {text}
-              </Text>
-            </Box>
-          </Flex>
-        </ModalBody>
-      </ModalContent>
-    </Modal>
-  );
+    return (
+        <Modal size="5xl" isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalCloseButton />
+                <ModalBody>
+                    <Flex flexDir={{ base: "column", md: "row" }}>
+                        <Box w={{ base: "100%", md: "50%" }}>
+                            <TransformWrapper>
+                                <TransformComponent>
+                                    <Image alt="expandable image" src={url} />
+                                </TransformComponent>
+                            </TransformWrapper>
+                        </Box>
+                        <Box
+                            w={{ base: "100%", md: "50%" }}
+                            p="20px"
+                            minW={{ base: "0px", md: "400px" }}
+                        >
+                            <ButtonGroup mb="20px">
+                                <Button isDisabled={disableButton} onClick={handleImageScan}>
+                                    Escanear
+                                </Button>
+                            </ButtonGroup>
+                            <Text mb={"10px"} fontSize={"xl"}>
+                                <span style={{ fontWeight: "bold" }}>Nombre:</span> {imageName}
+                            </Text>
+                            <Text mb={"10px"} fontSize={"xl"}>
+                                <span style={{ fontWeight: "bold" }}>Número de factura:</span>
+                                {facturaNumber}
+                            </Text>
+                            <Text mb={"10px"} fontSize={"xl"}>
+                                <span style={{ fontWeight: "bold" }}>Texto:</span> {text}
+                            </Text>
+                        </Box>
+                    </Flex>
+                </ModalBody>
+            </ModalContent>
+        </Modal>
+    );
 };
 
 export default ImageEnlargeModal;
