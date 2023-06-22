2b821d29db3a6463bfeed13cbfc95cf176b7b54b
Tony Jara <340studiospy@gmail.com>
2023-06-22 13:51:17 -0300

    Added email sent on every request to approvers

--- server/trpc/routers/moneyApproval.routes.ts
+++ server/trpc/routers/moneyApproval.routes.ts
@@ -1,14 +1,20 @@
-import { z } from 'zod';
-import { adminModProcedure, router } from '../initTrpc';
-import prisma from '@/server/db/client';
-import { reqApprovalApprovedSlackNotification } from './notifications/slack/reqApprovalApproved.notification.slack';
-import { reqApprovalRejectionSlackNotification } from './notifications/slack/reqApprovalRejection.notification.slack';
+import { z } from "zod";
+import { adminModObserverProcedure, router } from "../initTrpc";
+import prisma from "@/server/db/client";
+import { reqApprovalApprovedSlackNotification } from "./notifications/slack/reqApprovalApproved.notification.slack";
+import { reqApprovalRejectionSlackNotification } from "./notifications/slack/reqApprovalRejection.notification.slack";
 
 export const moneyApprovalRouter = router({
-  approve: adminModProcedure
+  approve: adminModObserverProcedure
     .input(z.object({ moneyRequestId: z.string() }))
     .mutation(async ({ input, ctx }) => {
       const user = ctx.session.user;
+
+      await prisma?.organization.findFirstOrThrow({
+        where: {
+          moneyRequestApprovers: { some: { id: user.id } },
+        },
+      });
 
       const prevApproval = await prisma?.moneyRequestApproval.findFirst({
         where: { accountId: user.id, moneyRequestId: input.moneyRequestId },
@@ -18,9 +24,9 @@ export const moneyApprovalRouter = router({
         const approval = await prisma?.moneyRequestApproval.create({
           data: {
             moneyRequestId: input.moneyRequestId,
-            status: 'ACCEPTED',
+            status: "ACCEPTED",
             accountId: user.id,
-            rejectMessage: '',
+            rejectMessage: "",
           },
           include: {
             account: { select: { displayName: true } },
@@ -32,7 +38,7 @@ export const moneyApprovalRouter = router({
       }
 
       const approval = await prisma?.moneyRequestApproval.update({
-        data: { status: 'ACCEPTED', rejectMessage: '' },
+        data: { status: "ACCEPTED", rejectMessage: "" },
         include: {
           account: { select: { displayName: true } },
           moneyRequest: { select: { organizationId: true } },
@@ -42,17 +48,22 @@ export const moneyApprovalRouter = router({
       await reqApprovalApprovedSlackNotification({ input: approval });
       return approval;
     }),
-  reject: adminModProcedure
+  reject: adminModObserverProcedure
     .input(z.object({ moneyRequestId: z.string(), rejectMessage: z.string() }))
     .mutation(async ({ input, ctx }) => {
       const user = ctx.session.user;
 
+      await prisma?.organization.findFirstOrThrow({
+        where: {
+          moneyRequestApprovers: { some: { id: user.id } },
+        },
+      });
       const prevApproval = await prisma?.moneyRequestApproval.findFirst({
         where: { accountId: user.id, moneyRequestId: input.moneyRequestId },
       });
       if (prevApproval) {
         const approval = await prisma?.moneyRequestApproval.update({
-          data: { status: 'REJECTED', rejectMessage: input.rejectMessage },
+          data: { status: "REJECTED", rejectMessage: input.rejectMessage },
           include: {
             account: { select: { displayName: true } },
             moneyRequest: { select: { organizationId: true } },
@@ -65,7 +76,7 @@ export const moneyApprovalRouter = router({
       const approval = await prisma?.moneyRequestApproval.create({
         data: {
           moneyRequestId: input.moneyRequestId,
-          status: 'REJECTED',
+          status: "REJECTED",
           accountId: user.id,
           rejectMessage: input.rejectMessage,
         },
