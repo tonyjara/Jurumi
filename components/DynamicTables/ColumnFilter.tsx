48098da2202350beadffb6350d85af111cae08a2
Tony Jara <340studiospy@gmail.com>
2023-08-05 17:46:38 -0300

    added facturanumber filtering to expense reports

--- components/DynamicTables/ColumnFilter.tsx
+++ components/DynamicTables/ColumnFilter.tsx
@@ -11,52 +11,55 @@ import MoneyRequestStatusColumnFilter from "./ColumnFilters/MoneyRequestStatus.C
 import MoneyRequestTypeColumnFilter from "./ColumnFilters/MoneyRequestType.columnFilter";
 
 export interface ColumnFilterProps {
-  keyName?: string;
-  column: Column<any>;
-  whereFilterList: any[];
-  setWhereFilterList?: React.Dispatch<React.SetStateAction<any[]>>;
+    keyName?: string;
+    column: Column<any>;
+    whereFilterList: any[];
+    setWhereFilterList?: React.Dispatch<React.SetStateAction<any[]>>;
 }
 
 /** 
 This component lists filters that are specific to a column. They change the way the "where" query is executed inside prisma to give server filtering.
 */
 const ColumnFilter = (props: ColumnFilterProps) => {
-  const { column, setWhereFilterList, whereFilterList } = props;
+    const { column, setWhereFilterList, whereFilterList } = props;
 
-  return (
-    <div>
-      {column.id === "N." && (
-        <Button
-          leftIcon={<BsFilter />}
-          size={"sm"}
-          isDisabled={!whereFilterList.length}
-          onClick={() => setWhereFilterList && setWhereFilterList([])}
-        >
-          Borrar Filtros
-        </Button>
-      )}
-      {column.id === "status" && <MoneyRequestStatusColumnFilter {...props} />}{" "}
-      {column.id === "account_displayName" && (
-        <AccountDisplayNameColumnFilter {...props} />
-      )}
-      {column.id === "Proyecto" && (
-        <MoneyRequestProjectsColumnFilter {...props} />
-      )}{" "}
-      {column.id === "Linea Presupuestaria" && (
-        <MoneyRequestCostCategoriesColumnFilter {...props} />
-      )}{" "}
-      {column.id === "moneyRequestType" && (
-        <MoneyRequestTypeColumnFilter {...props} />
-      )}{" "}
-      {column.id === "description" && (
-        <InputContainsColumnFilter keyName="description" {...props} />
-      )}{" "}
-      {column.id === "comments" && (
-        <InputContainsColumnFilter keyName="comments" {...props} />
-      )}{" "}
-      {column.id === "createdAt" && <FromToDateColumnFilter {...props} />}{" "}
-    </div>
-  );
+    return (
+        <div>
+            {column.id === "N." && (
+                <Button
+                    leftIcon={<BsFilter />}
+                    size={"sm"}
+                    isDisabled={!whereFilterList.length}
+                    onClick={() => setWhereFilterList && setWhereFilterList([])}
+                >
+                    Borrar Filtros
+                </Button>
+            )}
+            {column.id === "status" && <MoneyRequestStatusColumnFilter {...props} />}{" "}
+            {column.id === "account_displayName" && (
+                <AccountDisplayNameColumnFilter {...props} />
+            )}
+            {column.id === "Proyecto" && (
+                <MoneyRequestProjectsColumnFilter {...props} />
+            )}{" "}
+            {column.id === "Linea Presupuestaria" && (
+                <MoneyRequestCostCategoriesColumnFilter {...props} />
+            )}{" "}
+            {column.id === "moneyRequestType" && (
+                <MoneyRequestTypeColumnFilter {...props} />
+            )}{" "}
+            {column.id === "description" && (
+                <InputContainsColumnFilter keyName="description" {...props} />
+            )}{" "}
+            {column.id === "comments" && (
+                <InputContainsColumnFilter keyName="comments" {...props} />
+            )}{" "}
+            {column.id === "facturaNumber" && (
+                <InputContainsColumnFilter keyName="facturaNumber" {...props} />
+            )}{" "}
+            {column.id === "createdAt" && <FromToDateColumnFilter {...props} />}{" "}
+        </div>
+    );
 };
 
 export default ColumnFilter;
