109e46f4ace5fd9f5ed07f8e7968ac42d7b25767
Tony Jara <340studiospy@gmail.com>
2023-08-22 12:12:18 -0300

    added partial print of rendiciones

--- lib/utils/ImageCompressor.ts
+++ lib/utils/ImageCompressor.ts
@@ -1,24 +1,22 @@
 import imageCompression from 'browser-image-compression';
 
 export const compressAvatar = async (file: File) => {
-  const options = {
-    maxSizeMB: 0.1,
-    maxWidthOrHeight: 480,
-    useWebWorker: true,
-  };
+    const options = {
+        maxSizeMB: 0.1,
+        maxWidthOrHeight: 480,
+        useWebWorker: true,
+    };
 
-  const compressedImage = await imageCompression(file, options);
+    const compressedImage = await imageCompression(file, options);
 
-  return compressedImage;
+    return compressedImage;
 };
 export const compressCoverPhoto = async (file: File) => {
-  const options = {
-    maxSizeMB: 0.2,
-    // maxWidthOrHeight: 480,
-    useWebWorker: true,
-  };
 
-  const compressedImage = await imageCompression(file, options);
+    return await imageCompression(file, {
+        maxSizeMB: 0.2,
+        // maxWidthOrHeight: 480,
+        useWebWorker: true,
+    });
 
-  return compressedImage;
 };
